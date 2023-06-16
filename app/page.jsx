d0a571c49af8409513ab049d30a2c63da69ec09e
darshil89 <darshilmahraur67@gmail.com>
2023-06-17 01:00:22 +0530

    nothing part 1

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
-const page = () => {
+import { getServerSession } from "next-auth"
+import { AuthOptions } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]/route"
+export default async function Home ()  {
+  const session=await getServerSession(authOptions)
   return (
     <div>
       <h1><b>hello</b></h1>
+      <h1>Server Side Rendering</h1>
+      
     </div>
   )
 }
 
-export default page
